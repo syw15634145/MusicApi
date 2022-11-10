58ce16b22197f8f08ecdbdaf3abcd2228260e14f
suyongwen <heyue2001@gmail.com>
2022-11-09 21:29:04 -0800

    1.0

---
+++ RestApp/Models/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RestApp.Models
+{
+	public class Song
+	{
+        public Song(string name, string artist)
+		{
+            Name = name;
+			Artist = artist;
+		}
+
+		public string Name { get; set; }
+
+		public string Artist { get; set; }
+    }
+}
+
